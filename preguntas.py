c4675b285f4f5a8900888208e3abf885586734b0
ALEJO\aleor <alejitoom5@gmail.com>
2022-06-27 12:25:26 -0500

    Entrega 7

--- preguntas.py
+++ preguntas.py
@@ -168,7 +168,7 @@ def pregunta_04():
 
     cfm_test = confusion_matrix(
         y_true=y_test,
-        y_pred=pipeline.predict(y_test),
+        y_pred=pipeline.predict(X_test),
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
